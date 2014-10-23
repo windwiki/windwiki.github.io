f28364da8b3c17f7de863f8c09bd125bb8940d5b
panyanhong <hzpanyh@corp.netease.com>
2014-10-23 21:56:07 +0800

    fix

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,1 @@
 console.log('This would be the main JS file.');
-js100(function(){
-    Javascript100.scroll({
-        box:"probox",
-        list:"prolist",
-        direction: "left",
-        advanceArrow:"left",
-        retreatArrow:"right"
-    });
-})
