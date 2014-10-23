9d8a86a30878a322759bad5e1538b075ad0ec526
panyanhong <hzpanyh@corp.netease.com>
2014-10-23 21:29:54 +0800

    add imgs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
 console.log('This would be the main JS file.');
+js100(function(){
+    Javascript100.scroll({
+        box:"probox",
+        list:"prolist",
+        direction: "left",
+        advanceArrow:"left",
+        retreatArrow:"right"
+    });
+})
