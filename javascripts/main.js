8e8514a2cfe5627ceeba2662246a45df259c8bc2
panyanhong <hzpanyh@corp.netease.com>
2014-10-24 16:43:42 +0800

    fix message

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,16 @@
 console.log('This would be the main JS file.');
 
-//document.write("<script src=‘../javascripts/jquery-2.1.1.min.js’></script>");
 $(document).ready(function(){
     $(".sh").click(function(){
+        var $pn=$(".sh p:eq(0)");
+        //var pn = document.getElementsByClassName("sh")[0].getElementsByTagName('p');
+        var nap = $pn.text();
+        console.log(nap);
+        if(nap==="OPEN"){
+            $pn.text("私の箱");
+        } else {
+            $pn.text("OPEN");
+        }
         $(".prob").slideToggle("slow");
     });
 });
