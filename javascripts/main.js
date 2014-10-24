ac543c2fe203a4943d6b1dd10c374d84c855c507
panyanhong <hzpanyh@corp.netease.com>
2014-10-24 15:47:39 +0800

    fix roll

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
 console.log('This would be the main JS file.');
+
+//document.write("<script src=‘../javascripts/jquery-2.1.1.min.js’></script>");
+$(document).ready(function(){
+    $(".sh").click(function(){
+        $(".prob").slideToggle("slow");
+    });
+});
