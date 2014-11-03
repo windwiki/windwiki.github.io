5c3b9ee8546e17138379d7921efc3d25ba263038
panyanhong <hzpanyh@corp.netease.com>
2014-11-03 20:48:53 +0800

    add localStorage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,65 @@
-console.log('This would be the main JS file.');
 
 $(document).ready(function(){
-    $(".sh").click(function(){
-        var $pn=$(".sh p:eq(0)");
-        //var pn = document.getElementsByClassName("sh")[0].getElementsByTagName('p');
-        var nap = $pn.text();
-        var $prob = $(".prob");
-        if(nap==="OPEN"){
-            $pn.text("私の箱");
-           // $prob.display("inherit");
-        } else {
-            $pn.text("OPEN");
-            //$prob.slideToggle("slow");
-        }
-        $prob.slideToggle("slow");
-    });
+    var $pn=$(".sh p:eq(0)");
+    checkLocalStorage($pn);
+
+    $pn.click({pn:$pn},toggle);
 });
+
+function toggle(event){
+    var $prob = $(".prob");
+    //var $pn = $(".sh p:eq(0)");
+    var pn = event.data.pn;
+    var nap = pn.text;
+
+    if (nap === "OPEN") {
+        pn.text("私の箱");
+        localStorage.setItem("open", "true");
+        // $prob.display("inherit");
+    } else {
+        pn.text("OPEN");
+        //$prob.slideToggle("slow");
+    }
+    $prob.slideToggle("slow");
+};
+function checkLocalStorage(pn){
+    var status = localStorage.getItem("open");
+    if(status&&status==="true"){
+       // var $pn=$(".sh p:eq(0)");
+        pn.text("私の箱");
+        var $prob = $(".prob");
+        //var prob = document.getElementsByClassName("prob")[0];
+        $prob.css("display","block");
+    }
+}
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload!='function'){
+        window.onload=func;
+    } else {
+        window.onload=function(){
+            oldonload;
+            func();
+        }
+    }
+}
 /*
+ $(document).ready(function(){
+ $(".sh").click(function(){
+ var $pn=$(".sh p:eq(0)");
+ //var pn = document.getElementsByClassName("sh")[0].getElementsByTagName('p');
+ var nap = $pn.text();
+ var $prob = $(".prob");
+ if(nap==="OPEN"){
+ $pn.text("私の箱");
+ // $prob.display("inherit");
+ } else {
+ $pn.text("OPEN");
+ //$prob.slideToggle("slow");
+ }
+ $prob.slideToggle("slow");
+ });
+ });
 $(document).ready(function(){
     $(".sdb").click(function(){
         console.log("enter");
