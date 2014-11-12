00ce5da919bb17ef74c02676b068d4ae118c47e8
windwiki <sredpan@gmail.com>
2014-11-12 14:51:44 +0800

    test markdown css

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,18 +8,14 @@ document.ready(function(){
 
 function toggle(event){
     var $prob = $(".prob");
-    //var $pn = $(".sh p:eq(0)");
     var pn = event.data.pn;
     var nap = pn.text();
-    //console.log(nap);
     if (nap === "OPEN") {
-
         pn.text("私の箱");
         localStorage.setItem("open", "true");
-        // $prob.display("inherit");
     } else {
         pn.text("OPEN");
-        //$prob.slideToggle("slow");
+        localStorage.setItem("open", "false");
     }
     $prob.slideToggle("slow");
 };
@@ -27,10 +23,8 @@ function checkLocalStorage(pn) {
 
     var status = localStorage.getItem("open");
     if (status && status === "true") {
-        // var $pn=$(".sh p:eq(0)");
         pn.text("私の箱");
         var $prob = $(".prob");
-        //var prob = document.getElementsByClassName("prob")[0];
         $prob.css("display", "block");
     }
 }
@@ -45,26 +39,3 @@ function addLoadEvent(func){
         }
     }
 }
-/*
- $(document).ready(function(){
- $(".sh").click(function(){
- var $pn=$(".sh p:eq(0)");
- //var pn = document.getElementsByClassName("sh")[0].getElementsByTagName('p');
- var nap = $pn.text();
- var $prob = $(".prob");
- if(nap==="OPEN"){
- $pn.text("私の箱");
- // $prob.display("inherit");
- } else {
- $pn.text("OPEN");
- //$prob.slideToggle("slow");
- }
- $prob.slideToggle("slow");
- });
- });
-$(document).ready(function(){
-    $(".sdb").click(function(){
-        console.log("enter");
-        $(".prob").display("inherit");
-    });
-});*/
